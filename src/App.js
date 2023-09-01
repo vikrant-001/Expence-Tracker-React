ae52c441155d8df2012e0a03dadcf1b26e223ce9
vikrant-001 <121452566+vikrant-001@users.noreply.github.com>
2023-09-01 17:45:55 +0530

    welcome Page added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import { Fragment } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import LoginSignUp from './components/AuthPages/LoginSignUp';
+import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
 
 function App() {
+  const [isloggin,setLogin] = useState(false);
+
+  const token = JSON.parse(localStorage.getItem('token'));
+  if(token){
+    setLogin(!isloggin);
+  }
   return (
-    <Fragment>
-      <LoginSignUp/>
-    </Fragment>
+    <Router>
+      <Routes>
+        <Route path='/' element = {isloggin ? <LoginSignUp/> : <p>Welcom To Epence tracker</p>}>
+
+        </Route>
+      </Routes>
+    </Router>
   )
 }
 
--- src/components/AuthPages/LoginSignUp.jsx
+++ src/components/AuthPages/LoginSignUp.jsx
@@ -10,6 +10,34 @@ const LoginSignUp = () => {
     const toggleHandler = () => {
         setLogin(!login);
     }
+    const submitHandler2 = async (event) => {
+        event.preventDefault();
+        if(!emailRef.current.value || !passwordRef.current.value){
+            alert("Please Enter All Details");
+            return;
+        }
+
+        try{
+            const response = await fetch('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCJHhjWfvJsQgX9BVv3VbjxgZTJMV2_9rE',{
+                method:'POST',
+                body:JSON.stringify({
+                    email:emailRef.current.value,
+                    password:passwordRef.current.value,
+                    returnSecureToken:true,
+                }),
+                headers:{
+                    "Content-Type": "application/json",
+                }
+            });
+
+            const data = await response.json();
+            
+        }
+
+        catch(error){
+
+        }
+    }
 
     const submitHandler = async (event) => {
         event.preventDefault();
